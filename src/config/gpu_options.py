ce7edd9eedf57301d2da9629d1530f7488a237c3
Jose Lucas Silva Mayer <josemayer@usp.br>
2023-10-11 13:44:24 -0300

    Return GPU config
    Signed-off-by: Jose Lucas Silva Mayer <josemayer@usp.br>

--- src/config/gpu_options.py
+++ src/config/gpu_options.py
@@ -3,3 +3,5 @@ import tensorflow as tf
 def gpu_config():
     gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.5)
     config = tf.compat.v1.ConfigProto(gpu_options=gpu_options)
+
+    return config
