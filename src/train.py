574f1ab6a7fb43901d8839e04cf97f48e5785584
José Lucas Silva Mayer <josemayer@usp.br>
2023-11-24 06:44:54 -0300

    Add fit after tuning with new network hyperparameters
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/train.py
+++ src/train.py
@@ -8,7 +8,7 @@ import keras_tuner as kt
 from transformers import BertTokenizer, TFBertModel
 from tensorflow.keras.layers import Input, Dense, Dropout
 from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import LambdaCallback, Callback
+from tensorflow.keras.callbacks import LambdaCallback, Callback, EarlyStopping
 from config.gpu_options import gpu_config
 
 def normalize_grades(grades):
@@ -150,6 +150,15 @@ def main():
 
     best_model_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
     best_model = tuner.hypermodel.build(best_model_hps)
+
+    best_model.fit(
+        np.array(train_encodings['input_ids']),
+        train_labels,
+        validation_data=(np.array(valid_encodings['input_ids']), valid_labels),
+        epochs=15,
+        batch_size=best_model_hps.get('batch_size'),
+        callbacks=[EarlyStopping(monitor='val_loss', patience=3)]
+    )
 
     evaluation = best_model.evaluate(np.array(test_encodings['input_ids']), test_labels)
     print("Evaluation results:", evaluation)
